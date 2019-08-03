d6b1cf06fdbad659b03c9fc1bafe25f5818516ec
clhuang224 <clhuang224@gmail.com>
2019-08-03 22:12:47 +0800

    Finish player layout

--- W3_MP3/js/all.js
+++ W3_MP3/js/all.js
@@ -2,7 +2,13 @@ $(document).ready(function(){
     $('.aside-switch').click(function(){
         $('.main').toggleClass('hide-aside');
     });
-    
-
-
+    $('.player .play-button').click(function(){
+        $('.player').toggleClass('pause');
+    });
+    $('.player .random-button').click(function(){
+        $('.player').toggleClass('random');
+    });
+    $('.player .mute-button').click(function(){
+        $('.player').toggleClass('mute');
+    });
 });
