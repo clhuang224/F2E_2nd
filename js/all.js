b075c7930b03ba6c678ebe4dc3f9ff6ed3ec3464
clhuang224 <clhuang224@gmail.com>
2019-08-10 16:47:22 +0800

    Update index

--- js/all.js
+++ js/all.js
@@ -1,6 +1,16 @@
-$('#list-tab>li>a').click(function(event){
+$('#list-tab>li>a').click(function (event) {
     event.preventDefault();
     $(this).tab('show');
     $(this).parent().addClass('active');
     $(this).parent().siblings().removeClass('active');
 });
+
+let href = window.location.href;
+if (href.search('#') !== -1){
+    href = href.slice(href.search('#')+1);
+}
+else{
+    href = 'w1';
+}
+document.querySelector(`.main #list-tab a[href="#${href.toLowerCase()}"]`).parentElement.classList.add('active');
+document.querySelector(`.main .tab-content .tab-pane[id="${href.toLowerCase()}"]`).classList.add('active');
