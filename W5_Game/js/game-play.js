180c8adddb0b446b6ab2e839b7e78e46415ebefd
clhuang224 <clhuang224@gmail.com>
2019-08-13 17:22:29 +0800

    Update W5_Game images

--- W5_Game/js/game-play.js
+++ W5_Game/js/game-play.js
@@ -1,11 +1,24 @@
 const gamePlay = {
     key: 'gamePlay',
     preload: function () {
-        this.load.image('bg-back', '../img/bg-back.svg');
+        this.load.image('bg-back', '../img/bg-back.png');
+        this.load.image('bg-middle', '../img/bg-middle.png');
+        this.load.image('bg-front', '../img/bg-front.png');
+        this.load.image('bg-ground', '../img/bg-ground.png');
     },
     create: function () {
+        this.camera = this.cameras.add(0, 0, gameWidth, gameHeight);
+        this.camera.setBackgroundColor('#20552C');
+        this.backgroundColor = 0xFF0000;
         this.bgBack = this.add.tileSprite(gameWidth / 2, gameHeight / 2, gameWidth, gameHeight, 'bg-back');
+        this.bgMiddle = this.add.tileSprite(gameWidth / 2, gameHeight / 2, gameWidth, gameHeight, 'bg-middle');
+        this.bgFront = this.add.tileSprite(gameWidth / 2, gameHeight / 2, gameWidth, gameHeight, 'bg-front');
+        this.bgGround = this.add.tileSprite(gameWidth / 2, gameHeight / 2, gameWidth, gameHeight, 'bg-ground');
     },
     update: function () {
+        this.bgBack.tilePositionX += 1;
+        this.bgMiddle.tilePositionX += 3;
+        this.bgFront.tilePositionX += 5;
+        this.bgGround.tilePositionX += 6;
     }
 }
--- W5_Game/js/game-start.js
+++ W5_Game/js/game-start.js
@@ -3,18 +3,18 @@ let graphic;
 const gameStart = {
     key: 'gameStart',
     preload: function () {
-        this.load.image('img-title', '../img/img-title.svg');
-        this.load.image('text-title', '../img/txt-title.svg');
-        this.load.image('text-subtitle', '../img/txt-subtitle.svg');
-        this.load.image('button','../img/btn-press-start.svg');
+        this.load.image('img-title', '../img/img-title.png');
+        this.load.image('text-title', '../img/txt-title.png');
+        this.load.image('text-subtitle', '../img/txt-subtitle.png');
+        this.load.image('button','../img/btn-press-start.png');
     },
     create: function () {
         this.imgTitle = this.add.image(392.5, 38.75 + gameHeight / 2, 'img-title');
         graphic = this.add.graphics();
         graphic.fillStyle(0xffffff, 1);
-        graphic.fillRoundedRect(750, 32, 445, 736, 8)
+        graphic.fillRoundedRect(750, 32, 445, 736, 8);
         this.textTitle = this.add.image(791.52 + 359.08 / 2, 197.99 + 238.97 / 2, 'text-title');
-        this.textSubitle = this.add.image(846.41+272.14 / 2, 480.77 + 24 / 2, 'text-subtitle');
+        this.textSubtitle = this.add.image(846.41+272.14 / 2, 480.77 + 24 / 2, 'text-subtitle');
         this.button = this.add.image(979,592,'button');
     },
     update: function () {
