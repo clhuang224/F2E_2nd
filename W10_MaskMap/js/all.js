fbf7d3265a9c8548917e797badf7a3340ea6fde8
clhuang224 <clhuang224@gmail.com>
2020-02-10 17:10:09 +0800

    Update mark, search

--- W10_MaskMap/js/all.js
+++ W10_MaskMap/js/all.js
@@ -10,6 +10,35 @@ let vue = new Vue({
         distance: 1000,
         searching: false,
         geoloation: false,
+        icon: {
+            blue: L.icon({
+                iconUrl: '../img/mark-blue.png',
+                shadowUrl: '../img/shadow.png',
+                iconSize: [66, 90],
+                shadowSize: [58.5, 30], // size of the shadow
+                iconAnchor: [33, 90], // point of the icon which will correspond to marker's location
+                shadowAnchor: [0, 28],  // the same for the shadow
+                popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+            }),
+            grey: L.icon({
+                iconUrl: '../img/mark-grey.png',
+                shadowUrl: '../img/shadow.png',
+                iconSize: [66, 90],
+                shadowSize: [58.5, 30], // size of the shadow
+                iconAnchor: [33, 90], // point of the icon which will correspond to marker's location
+                shadowAnchor: [0, 28],  // the same for the shadow
+                popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+            }),
+            red: L.icon({
+                iconUrl: '../img/mark-red.png',
+                shadowUrl: '../img/shadow.png',
+                iconSize: [66, 90],
+                shadowSize: [58.5, 30], // size of the shadow
+                iconAnchor: [33, 90], // point of the icon which will correspond to marker's location
+                shadowAnchor: [0, 28],  // the same for the shadow
+                popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+            }),
+        },
     },
     filters: {
         round: function (value) {
@@ -51,8 +80,7 @@ let vue = new Vue({
             );
         },
         computeLevenshteinDistance(str1, str2) {
-            let matrix = new Array(str1.length +1, str2.length + 1);
-
+            let matrix = new Array(str1.length + 1, str2.length + 1);
 
         },
         getData: function () {
@@ -61,49 +89,80 @@ let vue = new Vue({
             xhr.open('get', 'https://raw.githubusercontent.com/kiang/pharmacies/master/json/points.json');
             xhr.onload = function () {
                 that.data = JSON.parse(xhr.responseText).features;
+                let markers = new L.markerClusterGroup().addTo(that.map);
+                for (let i = 0; i < that.data.length; i++) {
+
+                    // 處理 popup的內容
+                    let schedule = that.data[i].properties.available.split('、');
+                    let string = `<table class="schedule">
+                    <tr>
+                        <th></th><th>一</th><th>二</th><th>三</th><th>四</th><th>五</th><th>六</th><th>日</th>
+                    </tr>`;
+                    for (let j = 0; j < schedule.length; j++) {
+                        switch (j) {
+                            case 0:
+                                string += `<tr><th>早上</th>`;
+                            case 7:
+                                string += `<tr><th>下午</th>`;
+                            case 14:
+                                string += `<tr><th>晚上</th>`;
+                            default:
+                                if (schedule[j].indexOf('看診') > 0) {
+                                    string += `<td>O</td>`;
+                                }
+                                else {
+                                    string += `<td>X</td>`;
+                                }
+
+                        }
+                        if (j % 7 === 6) {
+                            string += `</tr>`;
+                        }
+                    }
+                    string += `</table>`;
+                    let popupString = `
+                    <h2 class="title">${that.data[i].properties.name}</h2>
+                    ${string}
+                    `;
+
+                    // 放入 mark
+                    if (that.data[i].properties.mask_adult > 0 || that.data[i].properties.mask_child > 0) {
+                        markers.addLayer(L.marker(
+                            [that.data[i].geometry.coordinates[1], that.data[i].geometry.coordinates[0]],
+                            { icon: that.icon.blue }
+                        ).bindPopup(popupString));
+                    }
+                    else {
+                        markers.addLayer(L.marker(
+                            [that.data[i].geometry.coordinates[1], that.data[i].geometry.coordinates[0]],
+                            { icon: that.icon.grey }
+                        ).bindPopup(popupString));
+                    }
+
+                }
+                that.map.addLayer(markers);
             };
             xhr.send();
         },
         openMap: function () {
+            // 設定地圖
+            this.map = L.map('map', {
+                center: this.center,
+                zoom: this.zoom
+            });
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                { attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>' }).addTo(this.map);
+            // 定位
             if (navigator.geolocation) {
                 let that = this;
                 navigator.geolocation.getCurrentPosition(
                     // 有使用者定位
                     function (position) {
+                        // 設定 map
                         that.geoloation = true;
                         that.center = [position.coords.latitude, position.coords.longitude];
                         that.zoom = 17;
-                        that.map = L.map('map', {
-                            center: that.center,
-                            zoom: that.zoom
-                        });
-                        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-                            { attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>' }).addTo(that.map);
-                        that.userInput = '您的位置';
-                        let markers = new L.markerClusterGroup();
-                        for (let i = 0; i < that.data.length; i++) {
-                            markers.addLayer(L.marker(
-                                [that.data[i].geometry.coordinates[1], that.data[i].geometry.coordinates[0]]));
-                        }
-                        that.map.addLayer(markers);
-                    },
-                    // 無使用者定位
-                    function (err) {
-                        if (err.code === 1) {
-                            that.map = L.map('map', {
-                                center: that.center,
-                                zoom: that.zoom
-                            });
-                            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-                                { attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>' }).addTo(that.map);
-                        }
-                        that.userInput = that.center;
-                        let markers = new L.markerClusterGroup();
-                        for (let i = 0; i < that.data.length; i++) {
-                            markers.addLayer(L.marker(
-                                [that.data[i].geometry.coordinates[1], that.data[i].geometry.coordinates[0]]));
-                        }
-                        that.map.addLayer(markers);
+                        that.map.setView(new L.LatLng(that.center[0], that.center[1]), that.zoom);
                     }
                 );
             }
@@ -122,11 +181,9 @@ let vue = new Vue({
             else {
                 return true;
             }
-
         },
-        changePosition(item){
-            if(item.type === 'self')
-            {
+        changePosition(item) {
+            if (item.type === 'self') {
                 if (navigator.geolocation) {
                     let that = this;
                     navigator.geolocation.getCurrentPosition(
@@ -135,24 +192,22 @@ let vue = new Vue({
                             that.center = [position.coords.latitude, position.coords.longitude];
                             that.zoom = 17;
                             that.map.setView(new L.LatLng(that.center[0], that.center[1]), that.zoom);
+                            that.searchInput = item.name;
                         },
                         // 無使用者定位
                         function (err) {
                             if (err.code === 1) {
                                 alert('您未提供位置資訊，請檢查您的瀏覽器設定。');
                             }
-                        
                         }
                     );
                 }
             }
         }
     },
-    beforeMount() {
-        this.getData();
-    },
     mounted() {
         this.openMap();
+        this.getData();
         let that = this;
         document.querySelector('body').addEventListener('click', function (event) {
             if (event.target.classList.contains('search-input') !== true) {
@@ -161,4 +216,3 @@ let vue = new Vue({
         });
     },
 });
-
